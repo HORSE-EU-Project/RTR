6c4722285fd0eec022e444bbff33692e3d304578
midan171 <mikedan171@gmail.com>
2025-07-21 19:13:29 +0300

    Remove deprecated files and clean up the repository structure

--- hashing.py
+++
@@ -1,7 +0,0 @@
-from passlib.context import CryptContext
-pwd_cxt = CryptContext(schemes =["bcrypt"],deprecated="auto")
-class Hash():
-   def bcrypt(password:str): #This method hashes the provided password using the bcrypt algorithm through the CryptContext object (pwd_cxt) and returns the hashed password.
-      return pwd_cxt.hash(password)
-   def verify(hashed,normal): #This method verifies whether the plain text password matches the hashed password.
-      return pwd_cxt.verify(normal,hashed)
